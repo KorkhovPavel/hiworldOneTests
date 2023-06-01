ee23cfff05ca10645697c32df73929aba6874d94
Pavel <korkhov.pi@gmail.com>
2023-06-01 16:13:50 +0300

    add test:open pages

--- conftest.py
+++ conftest.py
@@ -4,10 +4,15 @@ from playwright.sync_api import sync_playwright
 
 @pytest.fixture
 def browser_fixture():
+    """
+    open and close new windows browser
+    """
     with sync_playwright() as playwright:
+        # open new window browser
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
         yield page
+        # close window browser
         page.close()
         browser.close()
--- test_todo.py
+++ test_todo.py
@@ -1,8 +1,22 @@
+from time import sleep
+
 from playwright.sync_api import expect
+import requests
+import re
 
 
-def test_home_page_open(page):
-    page.goto('https://hiworld.one')
-    expect(page).to_have_url("https://hiworld.one/")
+def test_pages_open(page):
+    """
+    testing open pages
+    links from sitemap
+    """
+    item = requests.get('https://hiworld.one/sitemap.xml')
+    pattern = r'(?<=<loc>).{1,}(?=</loc>)'
+    links = re.findall(pattern, item.text)
+    for link in links:
+        page.goto(link)
+        expect(page).to_have_url(link)
+
+
 
 
