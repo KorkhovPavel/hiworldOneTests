df1e7015eec1d315b6695538b8469a9a8a580994
Pavel <korkhov.pi@gmail.com>
2023-06-02 16:55:14 +0300

    add test: test_search_post

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tests [hiworld.one](https://hiworld.one)
+
+1. Testing open pages
+2. Check search posts
+
--- test_one.py
+++ test_one.py
@@ -1,3 +1,4 @@
+import time
 from time import sleep
 
 from playwright.sync_api import expect
@@ -7,8 +8,8 @@ import re
 
 def test_pages_open(page):
     """
-    testing open pages
-    links from sitemap
+    Testing open pages.
+    Links from sitemap.
     """
     item = requests.get('https://hiworld.one/sitemap.xml')
     pattern = r'(?<=<loc>).{1,}(?=</loc>)'
@@ -18,5 +19,18 @@ def test_pages_open(page):
         expect(page).to_have_url(link)
 
 
-
+def test_search_post(page):
+    """
+    Testing search posts.
+    On pages(links) from sitemap.
+    """
+    item = requests.get('https://hiworld.one/sitemap.xml')
+    pattern = r'(?<=<loc>).{1,}(?=</loc>)'
+    links = re.findall(pattern, item.text)
+    for link in links:
+        page.goto(link)
+        page.get_by_placeholder("Enter value...").click()
+        page.get_by_placeholder("Enter value...").fill("Sitemap")
+        page.get_by_placeholder("Enter value...").press("Enter")
+        expect(page.get_by_text("Python Flask Sitemap Example")).to_be_visible()
 
