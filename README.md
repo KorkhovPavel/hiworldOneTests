138da6c6f40b041f67b096c3d1412e76e00ed966
Pavel <korkhov.pi@gmail.com>
2023-06-05 22:04:27 +0300

    add test: test_title_pages

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 1. Testing open pages
 2. Check search posts
-
+3. Check title to pages
--- test_one.py
+++ test_one.py
@@ -1,9 +1,16 @@
-import time
-from time import sleep
-
 from playwright.sync_api import expect
 import requests
 import re
+
+
+def get_sitemap_links():
+    """
+    get links from sitemap
+    :return: list links
+    """
+    sitemap = requests.get('https://hiworld.one/sitemap.xml')
+    pattern = r'(?<=<loc>).{1,}(?=</loc>)'
+    return re.findall(pattern, sitemap.text)
 
 
 def test_pages_open(page):
@@ -11,10 +18,7 @@ def test_pages_open(page):
     Testing open pages.
     Links from sitemap.
     """
-    item = requests.get('https://hiworld.one/sitemap.xml')
-    pattern = r'(?<=<loc>).{1,}(?=</loc>)'
-    links = re.findall(pattern, item.text)
-    for link in links:
+    for link in get_sitemap_links():
         page.goto(link)
         expect(page).to_have_url(link)
 
@@ -24,13 +28,36 @@ def test_search_post(page):
     Testing search posts.
     On pages(links) from sitemap.
     """
-    item = requests.get('https://hiworld.one/sitemap.xml')
-    pattern = r'(?<=<loc>).{1,}(?=</loc>)'
-    links = re.findall(pattern, item.text)
-    for link in links:
+    for link in get_sitemap_links():
         page.goto(link)
         page.get_by_placeholder("Enter value...").click()
         page.get_by_placeholder("Enter value...").fill("Sitemap")
         page.get_by_placeholder("Enter value...").press("Enter")
         expect(page.get_by_text("Python Flask Sitemap Example")).to_be_visible()
 
+
+def get_title_page(link):
+    """
+    From link  - get title
+    :param link: link page
+    :return: title
+    """
+    title_replace = {'Postgresql': 'PostgreSQL', 'Github': 'GitHub'}
+    title = link.split('/')[-1].replace('-', ' ').title()
+    for k, i in title_replace.items():
+        if k in title:
+            title = title.replace(k, i)
+    return title
+
+
+def test_title_pages(page):
+    """
+    Testing title pages.
+    On pages(links) from sitemap.
+    """
+    for link in get_sitemap_links():
+        page.goto(link)
+        title = get_title_page(link)
+        if title == '':
+            title = title.replace('', 'hiWorld.one')
+        expect(page).to_have_title(title)
